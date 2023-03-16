1d8063f37331f135359450b02a115109053d4b7c
S.P. ROY <sprithi44@gmail.com>
2023-03-16 19:31:29 -0400

    Minor changes to various seeds and sql file

--- seeds/product-seeds.js
+++ seeds/product-seeds.js
@@ -2,32 +2,32 @@ const { Product } = require('../models');
 
 const productData = [
   {
-    product_name: 'Plain T-Shirt',
+    product_name: 'Plaid T-Shirt',
     price: 14.99,
     stock: 14,
     category_id: 1,
   },
   {
-    product_name: 'Running Sneakers',
+    product_name: 'Hopping Sneakers',
     price: 90.0,
     stock: 25,
     category_id: 5,
   },
   {
-    product_name: 'Branded Baseball Hat',
+    product_name: 'Breaded Baseball Hat',
     price: 22.99,
     stock: 12,
     category_id: 4,
   },
   {
-    product_name: 'Top 40 Music Compilation Vinyl Record',
+    product_name: 'Down 40 Music Compilation Vinyl Record',
     price: 12.99,
     stock: 50,
     category_id: 3,
   },
   {
-    product_name: 'Cargo Shorts',
-    price: 29.99,
+    product_name: 'Prada Loincloth',
+    price: 99.69,
     stock: 22,
     category_id: 2,
   },
