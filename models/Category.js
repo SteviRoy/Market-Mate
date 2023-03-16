13bf45975f0d6dd0bdf8a9016efe5b9fd37f624e
S.P. ROY <sprithi44@gmail.com>
2023-03-16 19:37:30 -0400

    Added comments to Catagories.js

--- models/Category.js
+++ models/Category.js
@@ -1,12 +1,13 @@
 const { Model, DataTypes } = require('sequelize');
-
 const sequelize = require('../config/connection.js');
 
+// Create a Category class that extends Model from Sequelize
 class Category extends Model {}
 
+// Initialize the Category model with its properties and options
 Category.init(
   {
-    // define columns
+    // Define the columns of the Category model
     id: {
       type: DataTypes.INTEGER,
       allowNull: false,
@@ -19,12 +20,18 @@ Category.init(
     },
   },
   {
+    // Provide the connection instance to the Category model
     sequelize,
+    // Disable timestamps (createdAt and updatedAt)
     timestamps: false,
+    // Prevent Sequelize from transforming table names into plural
     freezeTableName: true,
+    // Enable underscored naming convention for automatically generated fields
     underscored: true,
+    // Set the name of the model for Sequelize to use internally
     modelName: 'category',
   }
 );
 
+// Export the Category model for use in other parts of the application
 module.exports = Category;
