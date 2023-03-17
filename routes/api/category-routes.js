5832e8d8ff19f64479bbce1371c414e1820f33f1
S.P. ROY <sprithi44@gmail.com>
2023-03-16 20:45:14 -0400

    Find one catagorie by id added

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -16,9 +16,23 @@ router.get('/', async (req, res) => {
   }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const category = await Category.findByPk(req.params.id, {
+      include: [Product],
+    });
+
+    if (!category) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(category);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
